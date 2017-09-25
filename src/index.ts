7883aebdde37616704b7e67b0806af33b18d1149
Nathan Files <nathanwfiles@gmail.com>
2017-09-25 09:12:42 -0400

    converts to typescript
    - adds simple build step for compiling typescript
    - adds "bin" definition for `resize-img` command. Eventually other useful commands will be added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-const commandLineArgs = require('command-line-args');
-const fs = require('fs');
-const path = require('path');
+#! /usr/bin/env node
 
-const { resizeImageBuffer, resizeImageFile } = require('./resize');
+import * as commandLineArgs from 'command-line-args';
+import * as fs from 'fs';
+import * as path from 'path';
+import { resizeImageBuffer, resizeImageFile } from './resize';
+
+enum ExitCode {
+    Success = 0,
+    OutIsntDirectory = 1,
+    InIsntDirectory = 2
+}
 
 const cliArgsDefinition = [
     { name: 'width', alias: 'w', type: Number },
@@ -35,23 +42,23 @@ if (stdio) {
     });
     process.stdin.on('end', () => {
         const image = Buffer.concat(chunks);
-        resizeImageBuffer(MAX_WIDTH, MAX_HEIGHT, OUT_DIR)(image)
+        resizeImageBuffer(MAX_WIDTH, MAX_HEIGHT, image)
             .then(buffer => process.stdout.write(buffer))
             .catch(err => console.error(err));
     });
 } else {
     if (fs.existsSync(OUT_DIR)) {
-        if (!fs.statSync(OUT_DIR).isDirectory) {
+        if (!fs.statSync(OUT_DIR).isDirectory()) {
             console.error(`--out must be a directory: '${OUT_DIR}'`);
-            return;
+            process.exit(ExitCode.OutIsntDirectory);
         }
     } else {
         fs.mkdirSync(OUT_DIR);
     }
 
     if (!fs.existsSync(IN_DIR) || !fs.statSync(IN_DIR).isDirectory()) {
-        console.error('--in must be a valid directory')
-        return;
+        console.error('--in must be a valid directory');
+        process.exit(ExitCode.InIsntDirectory);
     }
 
     const files = fs.readdirSync(IN_DIR)
@@ -61,7 +68,7 @@ if (stdio) {
             if (verbose) {
                 console.log(` * Resizing: ${file}`);
             }
-            return resizeImageFile(MAX_WIDTH, MAX_HEIGHT, OUT_DIR)(file);
+            return resizeImageFile(MAX_WIDTH, MAX_HEIGHT, OUT_DIR, file);
         });
 
     Promise.all(files)
