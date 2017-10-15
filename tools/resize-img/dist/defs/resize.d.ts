079a5378a68793d13ebc4a15f3241f20536e62f6
Nathan Files <nathanwfiles@gmail.com>
2017-10-15 19:49:49 -0400

    ignores dist directories in git

--- tools/resize-img/dist/defs/resize.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="sharp" />
-/// <reference types="node" />
-import * as sharp from 'sharp';
-export declare const resizeImageFile: (maxWidth: number, maxHeight: number, out: string, file: string) => Promise<sharp.OutputInfo>;
-export declare const resizeImageBuffer: (maxWidth: number, maxHeight: number, buffer: Buffer) => Promise<Buffer>;
